d30a771df62d79f8e46c05d4f5d12cb139ec2547
Roee Chen <roeechen01@gmail.com>
2019-04-13 18:47:40 +0300

    Added Boss.cs - inherits zombie, base to the bosses

---
+++ Assets/Scripts/Zombie/Boss.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Boss : Zombie
+{
+    private Slider slider;
+
+    new void Start()
+    {
+        base.Start();
+        slider = FindObjectOfType<Slider>();
+        slider.maxValue = this.life;
+        slider.value = this.life;
+        this.boss = true;
+    }
+
+    public override void HeadShot(int demage)
+    {
+        Hit(demage * 2);
+    }
+
+    public override void Hit(int demage)
+    {
+        base.Hit(demage);
+        UpdateSlider();
+    }
+
+    void UpdateSlider()
+    {
+        InvokeRepeating("SliderFlow", 0f, 0.0075f);
+    }
+
+    void SliderFlow()
+    {
+        slider.value -= 0.025f;
+        if (slider.value <= this.life)
+        {
+            slider.value = this.life;
+            CancelInvoke("SliderFlow");
+        }
+        if (slider.value <= 0)
+            base.Dead();
+    }
+
+    public override void Dead() { }
+}
--- Assets/Scripts/Zombie/KnifeBossZombie.cs
+++ Assets/Scripts/Zombie/KnifeBossZombie.cs
@@ -3,56 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class KnifeBossZombie : Zombie
+public class KnifeBossZombie : Boss
 {
-    private Slider slider;
-
-    new void Start()
-    {
-        base.Start();
-        slider = FindObjectOfType<Slider>();
-        slider.maxValue = this.life;
-        slider.value = this.life;       
-    }
-
     protected override void CreateZombie()
     {
         CreateZombieTypeInfo(100, 30, 3f);
-        this.boss = true;
     }
-
-    public override void HeadShot(int demage)
-    {
-        Hit(demage * 2);
-    }
-
-    public override void Hit(int demage)
-    {
-        base.Hit(demage);
-        UpdateSlider();
-    }
-
-    void UpdateSlider()
-    {
-        InvokeRepeating("SliderFlow", 0f, 0.005f);
-    }
-
-    void SliderFlow()
-    {
-        slider.value -= 0.02f;
-        if(slider.value <= this.life)
-        {
-            slider.value = this.life;
-            CancelInvoke("SliderFlow");
-        }
-        if (slider.value <= 0)
-            base.Dead();
-    }
-
-    public override void Dead()
-    {
-        //base.Dead();
-    }
-
-
 }
