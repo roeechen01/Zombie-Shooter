97e3ff3057c5e72fb931148fedd45bf51de69f72
Roee Chen <roeechen01@gmail.com>
2019-04-21 21:44:57 +0300

    The boss zombie now has a special ability- every hit it does- it's getting bigger&stronger&faster (faster in the ghost mode that it now has)

--- Assets/Scripts/Zombie/KnifeBossZombie.cs
+++ Assets/Scripts/Zombie/KnifeBossZombie.cs
@@ -5,8 +5,75 @@ using UnityEngine.UI;
 
 public class KnifeBossZombie : Boss
 {
+    private bool visible = true;
+    private SpriteRenderer sr;
+    private float speedFactor = 1.4f;
+    private bool toggleStarted = false;
+
     protected override void CreateZombie()
     {
-        CreateZombieTypeInfo(120, 30, 4.5f);
+        CreateZombieTypeInfo(120, 30, 4f);
+    }
+
+    void ToggleVisibility()
+    {
+        if (visible)
+        {
+            MakeInvisible();
+        }
+        else
+        {
+            MakeVisible();
+        }
+    }
+
+    void OnBecameVisible()
+    {
+        if (!toggleStarted)
+        {
+            toggleStarted = true;
+            sr = GetComponent<SpriteRenderer>();
+            InvokeRepeating("ToggleVisibility", 1f, 2f);
+        }
+    }
+
+    void MakeVisible()
+    {
+        canHit = true;
+        visible = true;
+        sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, 1f);
+        this.speed  = 4f;
+    }
+
+    void MakeInvisible()
+    {
+        canHit = false;
+        visible = false;
+        sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, 0.2f);
+        this.speed *= this.speedFactor;
+    }
+
+    protected override void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if (collider2D.gameObject.tag.Equals("Player") && player.body == collider2D)
+        {
+            if (player.GetVulnerable())
+            {
+                DemagingPlayer();
+                this.transform.localScale = new Vector3(transform.localScale.x * 1.2f, transform.localScale.y * 1.2f);
+                demage += 10f;
+                speedFactor += 0.2f;
+                rigidBody2d.constraints = RigidbodyConstraints2D.FreezeAll;
+            }
+        }
+
+        Bullet bullet = collider2D.gameObject.GetComponent<Bullet>();
+        if (bullet && canHit)
+        {
+            Blood tempBlood = Instantiate(blood, new Vector3(bullet.transform.position.x, bullet.transform.position.y, -1f), Quaternion.identity);
+            bloods.Add(tempBlood);
+            tempBlood.SetZombie(this);
+        }
+
     }
 }
