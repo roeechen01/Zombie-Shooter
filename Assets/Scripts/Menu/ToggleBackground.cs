e000ea2e0bd596ea4eba34aee46dfbb39979044d
Roee Chen <roeechen01@gmail.com>
2019-04-21 20:17:20 +0300

    Toggle blood background changes if mouse is over it or not.

---
+++ Assets/Scripts/Menu/ToggleBackground.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ToggleBackground : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Image background;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        background.color = new Color(1f, 1f, 1f, 0.8f);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        background.color = new Color(1f, 1f, 1f, 1f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        background = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
